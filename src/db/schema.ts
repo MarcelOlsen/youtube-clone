27eb699d67e5d12e22ef16ab64b1589810a5c61b
MarcelOlsen <marcelolsen26@gmail.com>
2025-02-09 00:02:28 +0100

    Configured neon and drizzle

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+
+// This will always be fired only by clerk, so we can throw the clerkId into here
+export const users = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  clerkId: text("clerk_id").unique().notNull(),
+  name: text("name").notNull(),
+  //TODO: add banner fields
+  imageUrl: text("image_url").notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull()
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)])
