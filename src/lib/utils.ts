3e7a38a37a02ec85574018970c2e757f97ae2fc6
MarcelOlsen <marcelolsen26@gmail.com>
2025-02-17 00:08:46 +0100

    Integrated with mux webhooks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatDuration = (duration: number): string => {
+  const seconds = Math.floor((duration % 60_000) / 1000)
+  const minutes = Math.floor(duration / 60_000)
+  return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+};
+
+export const snakeCaseToTitle = (str: string): string => {
+  return str.replace(/_/g, " ").replace(/\b\w/g, (match) => match.toUpperCase());
 }
--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { format } from "date-fns"
+import Link from "next/link"
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 
+import { InfiniteScroll } from "@/components/infinite-scroll"
 import {
   Table,
   TableBody,
@@ -12,10 +15,12 @@ import {
   TableRow
 } from "@/components/ui/table"
 
-import { InfiniteScroll } from "@/components/infinite-scroll"
 import { DEFAULT_LIMIT } from "@/constants"
+import { snakeCaseToTitle } from "@/lib/utils"
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail"
 import { trpc } from "@/trpc/client"
-import Link from "next/link"
+import { toSnakeCase } from "drizzle-orm/casing"
+import { Globe2Icon, LockIcon } from "lucide-react"
 
 export const VideosSection = () => {
   return (
@@ -54,16 +59,38 @@ const VideosSectionSuspense = () => {
               <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
                 <TableRow className="cursor-pointer">
                   <TableCell>
-                    {video.title}
+                    <div className="flex items-center gap-4">
+                      <div className="relative aspect-video w-36 shrink-0">
+                        <VideoThumbnail
+                          imageUrl={video.thumbnailUrl}
+                          previewUrl={video.previewUrl}
+                          title={video.title}
+                          duration={video.duration}
+                        />
+                      </div>
+                      <div className="flex flex-col overflow-hidden gap-y-1">
+                        <span className="text-sm line-clamp-1">{video.title}</span>
+                        <span className="text-xs text-muted-foreground line-clamp-1">{video.description || "No description"}</span>
+                      </div>
+                    </div>
                   </TableCell>
                   <TableCell>
-                    visibility
+                    <div className="flex items-center">
+                      {video.visibility === "private" ? (
+                        <LockIcon className="size-4 mr-2" />
+                      ) : (
+                        <Globe2Icon className="size-4 mr-2" />
+                      )}
+                      {toSnakeCase(video.visibility)}
+                    </div>
                   </TableCell>
                   <TableCell>
-                    status
+                    <div className="flex items-center">
+                      {snakeCaseToTitle(video.muxStatus || "error")}
+                    </div>
                   </TableCell>
-                  <TableCell>
-                    {video.createdAt.toLocaleDateString()}
+                  <TableCell className="text-sm truncate">
+                    {format(new Date(video.createdAt), "d MMM yyyy")}
                   </TableCell>
                   <TableCell className="text-right">
                     views
