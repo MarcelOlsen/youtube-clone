b8786a9819dab467da1ad557b128c3bb4a90f09d
MarcelOlsen <marcelolsen26@gmail.com>
2025-03-15 16:06:01 +0100

    Implemented liked playlist

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { db } from "@/db";
 import { users, videoReactions, videos, videoViews } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+
 export const playlistsRouter = createTRPCRouter({
   getHistory: protectedProcedure
     .input(z.object({
@@ -76,6 +77,89 @@ export const playlistsRouter = createTRPCRouter({
       const nextCursor = hasMore ? {
         id: lastItem.id,
         viewedAt: lastItem.viewedAt
+      } : null
+
+      return {
+        items,
+        nextCursor
+      };
+    }),
+  getLiked: protectedProcedure
+    .input(z.object({
+      cursor: z.object({
+        id: z.string().uuid(),
+        likedAt: z.date(),
+      }).nullish(),
+      limit: z.number().min(1).max(100),
+    }))
+    .query(async ({ ctx, input }) => {
+      const { id: userId } = ctx.user;
+      const { cursor, limit } = input
+
+      const viewerVideoReactions = db.$with("viewer_video_reactions").as(
+        db
+          .select({
+            videoId: videoReactions.videoId,
+            likedAt: videoReactions.updatedAt
+          })
+          .from(videoReactions)
+          .where(
+            and(
+              eq(videoReactions.userId, userId),
+              eq(videoReactions.type, "like")
+            )
+          )
+      )
+
+      const data = await db
+        .with(viewerVideoReactions)
+        .select({
+          ...getTableColumns(videos),
+          user: users,
+          likedAt: viewerVideoReactions.likedAt,
+          viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+          likeCount: db.$count(videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "like")
+            )
+          ),
+          dislikeCount: db.$count(videoReactions,
+            and(
+              eq(videoReactions.videoId, videos.id),
+              eq(videoReactions.type, "dislike")
+            )
+          )
+        }
+        )
+        .from(videos)
+        .innerJoin(users, eq(videos.userId, users.id))
+        .innerJoin(viewerVideoReactions, eq(viewerVideoReactions.videoId, videos.id))
+        .where(
+          and(
+            eq(videos.visibility, "public"),
+            cursor ? or(
+              lt(viewerVideoReactions.likedAt, cursor.likedAt),
+              and(
+                eq(viewerVideoReactions.likedAt, cursor.likedAt),
+                lt(videos.id, cursor.id)
+              )
+            ) : undefined
+          )
+        )
+        .orderBy(desc(viewerVideoReactions.likedAt), desc(videos.id))
+        .limit(limit + 1) // Checking if there's another video, to know if there's more data to fetch
+
+      const hasMore = data.length > limit;
+
+      // Removing the extra item, which we used to check if there's more data available
+      const items = hasMore ? data.slice(0, -1) : data
+
+      // The next cursor needs to be set to the "real" last item
+      const lastItem = items[items.length - 1]
+      const nextCursor = hasMore ? {
+        id: lastItem.id,
+        likedAt: lastItem.likedAt
       } : null
 
       return {
--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -3,7 +3,6 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { DEFAULT_LIMIT } from "@/constants";
-import { useIsMobile } from "@/hooks/use-mobile";
 import { trpc } from "@/trpc/client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
@@ -46,8 +45,6 @@ const ResultsSectionSkeleton = () => {
 }
 
 const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
-  const isMobile = useIsMobile()
-
   const [results, resultsQuery] = trpc.search.getMany.useSuspenseInfiniteQuery({
     query,
     categoryId,
@@ -58,23 +55,20 @@ const ResultsSectionSuspense = ({ query, categoryId }: ResultsSectionProps) => {
 
   return (
     <>
-      {isMobile ? (
-        <div className="flex flex-col gap-4 gap-y-10">
-          {results.pages
-            .flatMap((page) => page.items)
-            .map((video) => (
-              <VideoGridCard key={video.id} data={video} />
-            ))}
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {results.pages
-            .flatMap((page) => page.items)
-            .map((video) => (
-              <VideoRowCard key={video.id} data={video} />
-            ))}
-        </div>
-      )}
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {results.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard key={video.id} data={video} />
+          ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {results.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoRowCard key={video.id} data={video} />
+          ))}
+      </div>
       <InfiniteScroll
         hasNextPage={resultsQuery.hasNextPage}
         fetchNextPage={resultsQuery.fetchNextPage}
