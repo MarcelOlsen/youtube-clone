fa94bfefe10ad9e14ff3ab9f86cc26a384effd1c
MarcelOlsen <marcelolsen26@gmail.com>
2025-04-07 22:19:35 +0200

    Implemented banner upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -73,6 +73,52 @@ export const ourFileRouter = {
 
       return { uploadedBy: metadata.user.id };
     }),
+  bannerUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const { userId: clerkUserId } = await auth();
+
+      if (!clerkUserId) throw new UploadThingError("Unauthorized");
+
+      const [existingUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, clerkUserId))
+
+      if (!existingUser) throw new UploadThingError("Unauthorized");
+
+      if (existingUser.bannerKey) {
+        const utapi = new UTApi()
+
+        await utapi.deleteFiles(existingUser.bannerKey)
+        await db
+          .update(users)
+          .set({ bannerKey: null, bannerUrl: null })
+          .where(and(
+            eq(users.id, existingUser.id)
+          ));
+
+      }
+
+      return { userId: existingUser.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db
+        .update(users)
+        .set({
+          bannerUrl: file.ufsUrl,
+          bannerKey: file.key
+        })
+        .where(and(
+          eq(users.id, metadata.userId)
+        ));
+
+      return { uploadedBy: metadata.userId };
+    }),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -1,10 +1,12 @@
 import { useAuth } from "@clerk/nextjs";
 import { Edit2Icon } from "lucide-react";
+import { useState } from "react";
 
 import { UserGetOneOutput } from "../../types";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "./banner-upload-modal";
 
 interface UserPageBannerProps {
   user: UserGetOneOutput;
@@ -17,11 +19,16 @@ export const UserPageBannerSkeleton = () => {
 }
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
+  const [isBannerUploadOpen, setIsBannerUploadOpen] = useState(false);
   const { userId } = useAuth()
 
   return (
     <div className="relative group">
-      {/* TODO: add upload banner modal */}
+      <BannerUploadModal
+        userId={user.id}
+        open={isBannerUploadOpen}
+        onOpenChange={setIsBannerUploadOpen}
+      />
       <div
         className={cn(
           "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
@@ -33,6 +40,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
       >
         {user.clerkId === userId && (
           <Button
+            onClick={() => setIsBannerUploadOpen(true)}
             type="button"
             size="icon"
             className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
