d053a5dd484a259f3e6381c7cdfb47b35befc3ee
MarcelOlsen <marcelolsen26@gmail.com>
2025-04-08 22:38:08 +0200

    cors again

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' https://clerk.youtube.marcelolsen.dev https://challenges.cloudflare.com https://stream.mux.com https://e1b9ih0fkn.ufs.sh https://inferred.litix.io;
-  connect-src 'self' https://clerk.youtube.marcelolsen.dev https://stream.mux.com https://e1b9ih0fkn.ufs.sh https://inferred.litix.io;
-  img-src 'self' https://img.clerk.com;
-  worker-src 'self' blob:;
-  style-src 'self' 'unsafe-inline';
-  frame-src 'self' https://challenges.cloudflare.com;
-  form-action 'self';
-`
+// const cspHeader = `
+//   default-src 'self';
+//   script-src 'self' 'unsafe-inline' https://clerk.youtube.marcelolsen.dev https://challenges.cloudflare.com https://stream.mux.com https://e1b9ih0fkn.ufs.sh https://inferred.litix.io;
+//   connect-src 'self' https://clerk.youtube.marcelolsen.dev https://stream.mux.com https://e1b9ih0fkn.ufs.sh https://inferred.litix.io;
+//   img-src 'self' https://img.clerk.com;
+//   worker-src 'self' blob:;
+//   style-src 'self' 'unsafe-inline';
+//   frame-src 'self' https://challenges.cloudflare.com;
+//   form-action 'self';
+// `
 
 const nextConfig: NextConfig = {
   images: {
@@ -20,19 +20,19 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: cspHeader.replace(/\n/g, ''),
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
